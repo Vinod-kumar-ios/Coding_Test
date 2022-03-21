f9f04980651bb8ba2c0b4b30e795d8c334724206
Vinod Kumar <vinod.kumar@adglobal360.com>
2022-03-21 16:15:24 +0530

    UI design for cell

--- Coding_Test/Coding_Test/Views/NewsTableViewCell.swift
+++ Coding_Test/Coding_Test/Views/NewsTableViewCell.swift
@@ -12,21 +12,44 @@ class NewsTableViewCell: UITableViewCell {
     let containerStackView = StackView.init(alongAxis: .vertical, majorAxisSpacing: 15, minorAxisSpacing: 15)
     
     let newsImage = UIImageView()
-    let titleLable = TitleLabel(font: .boldSystemFont(ofSize: 16), textColor: .white)
-    let subTitleLable = SubTitleLabel(font: .boldSystemFont(ofSize: 16), textColor: .white)
+    let titleLabel = TitleLabel(font: .boldSystemFont(ofSize: 16), textColor: .black)
+    let subTitleLabel = SubTitleLabel(font: .systemFont(ofSize: 14), textColor: .black)
+    
+    public override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        prepareUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     
     func prepareUI() {
         selectionStyle = .none
         contentView.addSubview(containerStackView)
-        [newsImage, titleLable, subTitleLable].forEach {containerStackView.addArrangedSubview($0)}
-        subTitleLable.numberOfLines = 0
-        
+        [newsImage, titleLabel, subTitleLabel].forEach {containerStackView.addArrangedSubview($0)}
+        subTitleLabel.numberOfLines = 0
         
         // add constraints
         containerStackView.makeConstraintsWithContainer(contentView) { stackview, container in
             stackview.edges.equalTo(container.edges)
         }
+        newsImage.makeConstraintsWithSelf { $0.height.equalTo(150)}
+        newsImage.layer.cornerRadius = 10.0
+        newsImage.clipsToBounds = true
+        
     }
     
+}
+
+extension NewsTableViewCell{
+    func populateNews(article: Article?) {
+        guard let article = article else {
+            return
+        }
+        titleLabel.text = article.author
+        subTitleLabel.text = article.title
+        newsImage.loadImage(url: article.urlToImage ?? "")
+    }
 }
